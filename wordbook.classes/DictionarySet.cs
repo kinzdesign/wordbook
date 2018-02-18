6c827e4cced092d4b8ba3269f52ac76866145082
Kinsey Roberts <kinsdesign@gmail.com>
2018-02-18 12:57:29 -0500

    Stubbed DictionarySet.TryGetValues method and wrote a failing test

---
+++ wordbook.classes/DictionarySet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wordbook
+{
+    public class DictionarySet<K, V>
+    {
+        #region private variables
+
+        private Dictionary<K, HashSet<V>> dictionary = new Dictionary<K, HashSet<V>>();
+
+        #endregion
+
+        #region public functions
+
+        /// <summary>
+        /// Tries to find the HashSet associated with <paramref name="key"/>
+        /// </summary>
+        /// <param name="key">Key to search for</param>
+        /// <param name="values">HashSet<<typeparamref name="V"/>> to be populated if found</param>
+        /// <returns>True if <paramref name="key"/> was found and <paramref name="values"/> is populated; false otherwise</returns>
+        public bool TryGetValues(K key, out HashSet<V> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ wordbook.tests/DictionarySetTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wordbook.tests
+{
+    [TestClass]
+    public class DictionarySetTests
+    {
+        const int FOO = 1;
+        const int BAR = 2;
+
+        [TestMethod]
+        public void TestDictionarySetTryGetValues()
+        {
+            DictionarySet<int, int> dictionarySet = new DictionarySet<int, int>();
+            HashSet<int> hashSet;
+            Assert.AreEqual(false, dictionarySet.TryGetValues(FOO, out hashSet));
+        }
+    }
+}
