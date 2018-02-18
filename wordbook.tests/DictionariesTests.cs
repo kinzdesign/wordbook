7764fabada19ff0ba8969ee04ea330f63a05edf7
Kinsey Roberts <kinsdesign@gmail.com>
2018-02-18 14:44:36 -0500

    Refactored DictionariesTests expected lengths to a helper function

--- wordbook.tests/DictionariesTests.cs
+++ wordbook.tests/DictionariesTests.cs
@@ -9,39 +9,58 @@ namespace wordbook.tests
         [TestMethod]
         public void TestDictionariesGetSmall()
         {
-            TestDictionary(Dictionaries.Keys.very_small_test_dictionary, 12);
+            TestDictionary(Dictionaries.Keys.very_small_test_dictionary);
         }
 
         [TestMethod]
         public void TestDictionariesGetEight()
         {
-            TestDictionary(Dictionaries.Keys.eight_dictionary, 22339);
+            TestDictionary(Dictionaries.Keys.eight_dictionary);
         }
 
         [TestMethod]
         public void TestDictionariesGetQuarter()
         {
-            TestDictionary(Dictionaries.Keys.quarter_dictionary, 44674);
+            TestDictionary(Dictionaries.Keys.quarter_dictionary);
         }
 
         [TestMethod]
         public void TestDictionariesGetHalf()
         {
-            TestDictionary(Dictionaries.Keys.half_dictionary, 89346);
+            TestDictionary(Dictionaries.Keys.half_dictionary);
         }
 
         [TestMethod]
         public void TestDictionariesGetFull()
         {
-            TestDictionary(Dictionaries.Keys.dictionary, 93461); // TODO: 178692);
+            TestDictionary(Dictionaries.Keys.dictionary);
         }
 
-        private void TestDictionary(Dictionaries.Keys key, int expectedLength)
+        private void TestDictionary(Dictionaries.Keys key)
         {
             var dict = Dictionaries.Get(key);
             Assert.IsNotNull(dict, String.Format("Dictionary {0} is null", key));
             Assert.IsInstanceOfType(dict, typeof(string[]), String.Format("Dictionary {0} is not a string array", key));
+            int expectedLength = GetExpectedLength(key);
             Assert.AreEqual(expectedLength, dict.Length, String.Format("Dictionary {0} has wrong length. Expected {1}, is {2}.", key, expectedLength, dict.Length));
+        }
+
+        internal static int GetExpectedLength(Dictionaries.Keys key)
+        {
+            switch (key)
+            {
+                case Dictionaries.Keys.dictionary:
+                    return 93461; // TODO: 178692
+                case Dictionaries.Keys.eight_dictionary:
+                    return 22339;
+                case Dictionaries.Keys.half_dictionary:
+                    return 89346;
+                case Dictionaries.Keys.quarter_dictionary:
+                    return 44674;
+                case Dictionaries.Keys.very_small_test_dictionary:
+                    return 12;
+            }
+            return -1;
         }
     }
 }
