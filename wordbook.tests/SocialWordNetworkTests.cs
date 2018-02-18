0d8b991f04d6ed79e0c39ac955d5cbfe7c453ae8
Kinsey Roberts <kinsdesign@gmail.com>
2018-02-18 15:36:54 -0500

    Refactored SocialNetworkTests

--- wordbook.tests/SocialWordNetworkTests.cs
+++ wordbook.tests/SocialWordNetworkTests.cs
@@ -7,24 +7,35 @@ namespace wordbook.tests
     public class SocialWordNetworkTests
     {
         [TestMethod]
-        public void TestConstructors()
+        public void TestNetworks()
         {
-            TestConstructor(Dictionaries.Keys.dictionary);
-            TestConstructor(Dictionaries.Keys.eight_dictionary);
-            TestConstructor(Dictionaries.Keys.half_dictionary);
-            TestConstructor(Dictionaries.Keys.quarter_dictionary);
-            TestConstructor(Dictionaries.Keys.very_small_test_dictionary);
+            TestNetwork(Dictionaries.Keys.dictionary);
+            TestNetwork(Dictionaries.Keys.eight_dictionary);
+            TestNetwork(Dictionaries.Keys.half_dictionary);
+            TestNetwork(Dictionaries.Keys.quarter_dictionary);
+            TestNetwork(Dictionaries.Keys.very_small_test_dictionary);
         }
 
-        private void TestConstructor(Dictionaries.Keys key)
+        private void TestNetwork(Dictionaries.Keys key)
+        {
+            SocialWordNetwork network = TestConstructor(key);
+            TestWordCount(network);
+        }
+
+        private SocialWordNetwork TestConstructor(Dictionaries.Keys key)
         {
             SocialWordNetwork network = new SocialWordNetwork(key);
             // check keys match
             Assert.AreEqual(key, network.Key, String.Format("Key mismatch on creating SocialWordNetwork. Expected {0}, got {1}.", key, network.Key));
+            return network;
+        }
+
+        private void TestWordCount(SocialWordNetwork network)
+        {
             // check word count
-            int expectedWords = DictionariesTests.GetExpectedLength(key);
-            int actualWords = network.Words.Length;
-            Assert.AreEqual(expectedWords, actualWords, String.Format("Word count mismatch for dictionary '{0}'. Expected {1}, got {2}.", key, expectedWords, actualWords));
+            int expectedWords = DictionariesTests.GetExpectedLength(network.Key);
+            int actualWords = network.Words?.Length ?? 0;
+            Assert.AreEqual(expectedWords, actualWords, String.Format("Word count mismatch for dictionary '{0}'. Expected {1}, got {2}.", network.Key, expectedWords, actualWords));
         }
     }
 }
