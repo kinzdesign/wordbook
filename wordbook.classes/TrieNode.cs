06fe86f1124f8b2110b9ddb3a0988872e98e1cf1
Kinsey Roberts <kinsdesign@gmail.com>
2018-02-18 19:43:52 -0500

    Roughed in TrieNode class and tests

---
+++ wordbook.classes/TrieNode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace wordbook
+{
+    public class TrieNode
+    {
+        public readonly char Letter;
+
+        public readonly int? WordNumber;
+
+        private Dictionary<char, TrieNode> _children;
+
+        public TrieNode(char letter, int? wordNumber = null)
+        {
+            Letter = letter;
+            WordNumber = wordNumber;
+            _children = new Dictionary<char, TrieNode>();
+        }
+    }
+}
---
+++ wordbook.tests/TrieNodeTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wordbook.tests
+{
+    [TestClass]
+    public class TrieNodeTests
+    {
+        [TestMethod]
+        public void TestTrieNodeWithNumber()
+        {
+            TestTrieNode('K', 1);
+        }
+
+        [TestMethod]
+        public void TestTrieNodeWithoutNumber()
+        {
+            TestTrieNode('R');
+        }
+
+        private void TestTrieNode(char letter, int? wordNumber = null)
+        {
+            TrieNode node = new TrieNode(letter, wordNumber);
+            Assert.AreEqual(letter, node.Letter);
+            Assert.AreEqual(wordNumber, node.WordNumber);
+        }
+    }
+}
