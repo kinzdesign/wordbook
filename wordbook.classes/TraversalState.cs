9d77be02ff3014ec4a880d7c12ab9f1da54a4cf7
Kinsey Roberts <kinsdesign@gmail.com>
2018-02-19 19:07:56 -0500

    Added TrieNode.ToString()Added TraversalState class

---
+++ wordbook.classes/TraversalState.cs
@@ -0,0 +1,21 @@
+﻿namespace wordbook
+{
+    internal struct TraversalState
+    {
+        internal readonly int CharIndex;
+        internal readonly TrieNode Node;
+        internal readonly bool MadeEdit;
+
+        internal TraversalState(int charIndex, TrieNode node, bool madeEdit)
+        {
+            CharIndex = charIndex;
+            Node = node;
+            MadeEdit = madeEdit;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} at pos {1} - {2}edit made", Node.Letter, CharIndex, MadeEdit ? string.Empty : "NO ");
+        }
+    }
+}
